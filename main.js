0d0adbb12f9a65c5f4c59301268b045c957ee720
Sebastian R <ruanosebastian@gmail.com>
2020-07-01 15:28:49 -0700

    Add vue to the project and styled it
    Create calculator using VUE:
    Create Header, answer (which hold the answer being showed),deleteAnswer
    (which deletes the answer),and array of numbers, array of operators,
    equalAnswer(which is a button that will change the answer binding to
    the desired cualculation entered.)
    
    Methods:
    Use Regex to find characters.
    Add newAnswer = which will add the numbers to the answer binding.
    pickOperator = This picks the operator and replaced the operator if
    needs to be changed.
    deleteA = This will delete the last character from answer binding and
    the binding that separates the number after the operator.
    
    Things to add:
    A function that will return the answer to the answer binding.
    A function that will make sure I can only add one operator to the
    calculation.

---
+++ main.js
@@ -0,0 +1,90 @@
+let app = new Vue({
+    el: "#app",
+    data:{
+        header: 'The is my Vue Calculator',
+        answer : '',
+        deleteAnswer: 'DELETE',
+        numbers : [{
+            digitKey : 1,
+            digitNumber: '1'
+        },{
+            digitKey : 2,
+            digitNumber: '2'
+        },{
+            digitKey : 3,
+            digitNumber: '3'
+        },{
+            digitKey : 4,
+            digitNumber: '4'
+        },{
+            digitKey : 5,
+            digitNumber: '5'
+        },{
+            digitKey : 6,
+            digitNumber: '6'
+        },{
+            digitKey : 7,
+            digitNumber: '7'
+        },{
+            digitKey : 8,
+            digitNumber: '8'
+        },{
+            digitKey : 9,
+            digitNumber: '9'
+        },{
+            digitKey : 0,
+            digitNumber: '0'
+        }],
+        operators : [{
+            operatorKey : 111,
+            operatorSymbol : '+',
+            operatorName : 'plus'
+        },{
+            operatorKey : 112,
+            operatorSymbol : '-',
+            operatorName : 'minus'
+        },{
+            operatorKey : 113,
+            operatorSymbol : '/',
+            operatorName : 'divided'
+        },{
+            operatorKey : 114,
+            operatorSymbol : '*',
+            operatorName : 'multiplication'
+        }],
+        equalAnswer : '=',
+        firstNumbers : 1,
+        secondNumbers : [],
+        pickedOperator : ''
+    },
+    methods : {
+        newAnswer(x){
+            
+                    if(!this.answer.match(/[+*/=-]/gm)){
+                        this.answer = this.answer + x * 1 ;
+                        return this.firstNumbers = Number(this.answer)
+                    }else{
+                        console.log('boom');
+                        this.answer = this.answer + x * 1;
+                        this.secondNumbers.push(x)
+                    }
+            
+        },
+        pickOperator(x){
+                if(!this.answer.match(/[+*/=-]$/gm)){
+                    this.answer = this.answer + x;
+                    this.pickedOperator = x;
+                }else{
+                    this.answer = this.answer.replace(/[+*/=-]$/gm,x);
+                    this.pickedOperator = x;
+                }
+        },
+        deleteA(){
+            this.answer = this.answer.replace(/[0-9+*/=-]$/gm,'');
+            this.secondNumbers.pop()
+        }
+        
+    }
+});
+
+// If I click submit, I have to submit the information
